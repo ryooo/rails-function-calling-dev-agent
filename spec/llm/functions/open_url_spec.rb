ff745d70afb6006b177791c9a427b4f40a0b3e7b
ryooo <ryooo.321@gmail.com>
2023-09-13 20:53:39 +0900

    Update spec/llm/functions/open_url_spec.rb

--- spec/llm/functions/open_url_spec.rb
+++ spec/llm/functions/open_url_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Llm::Functions::OpenUrl do
   describe '#execute_and_generate_message' do
     context 'when the url is valid' do
-      let(:url) { 'https://www.example.com' }
+      let(:url) { 'https://www.google.co.jp/' }
       let(:what_i_want_to_know) { 'title' }
 
       it 'returns the correct result' do
